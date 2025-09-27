ef7d5a49b4b179877b85cef3336d308bbe7532be
Gogo <gogoshedrack@gmail.com>
2025-09-27 10:26:05 +0100

    Added simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,6 @@
+# simple_interest.py
+principal = 1000  # $1000
+rate = 0.05  # 5% annual interest rate
+time = 3  # 3 years
+interest = principal * rate * time
+print("The simple interest is:", interest)
