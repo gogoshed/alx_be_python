c23c3ca9bfc71d83577be8ba3d3222f3dfa96a73
Gogo <gogoshedrack@gmail.com>
2025-09-27 10:52:39 +0100

    Delete simple_interest.py

--- python_introduction/simple_interest.py
+++
@@ -1,6 +0,0 @@
-# simple_interest.py
-principal = 1000  # $1000
-rate = 0.05  # 5% annual interest rate
-time = 3  # 3 years
-interest = principal * rate * time
-print("The simple interest is:", interest)
