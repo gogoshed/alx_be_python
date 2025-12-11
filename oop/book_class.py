106fe5cab8cc76b8110100e4ab23e0b348b681e3
Gogo <gogoshedrack@gmail.com>
2025-12-11 14:20:43 +0100

    Fix submodule issue and add Book class with correct permissions

---
+++ oop/book_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Book class with magic methods
+"""
+
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        print("Deleting {}".format(self.title))
+
+    def __str__(self):
+        return "{} by {}, published in {}".format(self.title, self.author, self.year)
+
+    def __repr__(self):
+        return "Book('{}', '{}', {})".format(self.title, self.author, self.year)
